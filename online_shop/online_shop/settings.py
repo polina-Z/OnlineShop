5bf8c5f90d11d1628c88a14d5673d9780c1e54e5
Palina Zorka <palina.zorka@itechart-group.com>
2021-12-15 03:12:53 +0300

    switching the database to postgres

--- online_shop/online_shop/settings.py
+++ online_shop/online_shop/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'online_shop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'onlineshop',
+        'USER': 'shopuser',
+        'PASSWORD': 'p123456',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
