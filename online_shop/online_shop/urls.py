c588da21d5fa8d2dfe134f6d7154c549d5086067
Palina Zorka <palina.zorka@itechart-group.com>
2022-01-24 17:11:36 +0300

    update project structure

--- online_shop/online_shop/urls.py
+++ online_shop/online_shop/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf.urls.static import static
+from .settings import MEDIA_URL, MEDIA_ROOT
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('shop.urls')),
     path('api/', include('api.urls')),
-]
+] + static(MEDIA_URL, document_root=MEDIA_ROOT)
--- online_shop/shop/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import RegistrationView
-
-urlpatterns = [
-    # Auth
-    path('signup/', RegistrationView.as_view(), name='signup')
-]
